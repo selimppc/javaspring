0d84e27aa9d0edeef643f55d15a15d77cdffd92a
Selim.Reza <selim.reza>
2022-12-23 11:33:50 +0600

    filter config

--- src/main/java/demo/jwt/config/FilterConfig.java
+++ src/main/java/demo/jwt/config/FilterConfig.java
@@ -4,6 +4,7 @@ import demo.jwt.filter.JwtFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 @Configuration
 public class FilterConfig {
@@ -12,11 +13,17 @@ public class FilterConfig {
     public FilterRegistrationBean jwtFilter() {
         FilterRegistrationBean filter= new FilterRegistrationBean();
         filter.setFilter(new JwtFilter());
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         //provide endpoints which needs to be restricted.
         //All Endpoints would be restricted if unspecified
-        filter.addUrlPatterns("/api/v1/blog/restricted");
+        filter.addUrlPatterns(
+                "/api/v1/blog/restricted"
+                //"/api/v1/hello-hello"
+        );
         //filter.addUrlPatterns("/api/v1/*");
+
+
         return filter;
     }
 }
